151b83d5e00d843dc3b451c5b96ce3d7659a37b5
yz8bk <44685428+yz8bk@users.noreply.github.com>
2022-03-11 09:37:10 -0500

    Add files via upload

--- rcdt_hog_feature_extraction.py
+++ rcdt_hog_feature_extraction.py
@@ -21,7 +21,7 @@ def get_img_gradient(img, ksize):
     return sobelx, sobely
 
 
-def extract_rcdt_local(dx_blk, dy_blk, proj_n):
+def extract_drcdt_local(dx_blk, dy_blk, proj_n):
     THETA = proj_n
     if THETA == 2:
         THETA_range = np.asarray([30, 120])
@@ -36,15 +36,15 @@ def extract_rcdt_local(dx_blk, dy_blk, proj_n):
         THETA_range = np.linspace(0, 179, THETA) #[0, 90, 135] #180  # [0, 45, 90, 135]
 
     dxdy_blk_arry = np.vstack([dx_blk, dy_blk]) # shape: 2 * cellsize^2 (2*256)
-    my_rcdt_yzh = np.zeros([dx_blk.shape[0],THETA])
+    drcdt_local = np.zeros([dx_blk.shape[0],THETA])
     theta_cnt = 0
     for theta_temp in THETA_range:       
         w_local = np.asarray([np.cos((theta_temp*np.pi)/180), np.sin((theta_temp*np.pi)/180)]) # cos(theta), sin(theta) = 0: [1, 0], 180: [-1, 0]
         w_local_dot_dxdy_blk_arry = np.dot(w_local.reshape([1,2]), dxdy_blk_arry)
-        my_rcdt_yzh[:,theta_cnt] = np.sort(w_local_dot_dxdy_blk_arry) # sort ascending: x_0<x_1<...<x_n
+        drcdt_local[:,theta_cnt] = np.sort(w_local_dot_dxdy_blk_arry) # sort ascending: x_0<x_1<...<x_n
         theta_cnt = theta_cnt +1 
 
-    return my_rcdt_yzh
+    return drcdt_local
 
 
 class Patchify(object):
@@ -88,7 +88,7 @@ def get_drcdt_feature_sets(img_path_nm, step_size, overlap_size, proj_n):
     my_img_dy_blocks = rearrange(my_patch_dyy, '(b1 b2) h w -> b1 b2 h w', b1=int(np.sqrt(my_patch_dyy.shape[0])))
 
     
-    img_path0_myrcdt_local_img_test = extract_rcdt_local(my_img_dx_blocks[0,0,...].ravel(), my_img_dy_blocks[0,0,...].ravel(), proj_n)
+    img_path0_myrcdt_local_img_test = extract_drcdt_local(my_img_dx_blocks[0,0,...].ravel(), my_img_dy_blocks[0,0,...].ravel(), proj_n)
     
     myrcdt_local_h = img_path0_myrcdt_local_img_test.shape[0]
     myrcdt_local_w = img_path0_myrcdt_local_img_test.shape[1]
@@ -101,7 +101,7 @@ def get_drcdt_feature_sets(img_path_nm, step_size, overlap_size, proj_n):
         for n_col in range(col_STEPS):
             dx_temp = my_img_dx_blocks[n_row,n_col,...].ravel()/255. # scale the gradient value from [0 255] to [0, 1]
             dy_temp = my_img_dy_blocks[n_row,n_col,...].ravel()/255.
-            dxdy_myrcdt_local = extract_rcdt_local(dx_temp, dy_temp, proj_n)
+            dxdy_myrcdt_local = extract_drcdt_local(dx_temp, dy_temp, proj_n)
             big_Radon_img[n_row,n_col,:,:] = dxdy_myrcdt_local
 
     return my_img, my_img_dx_np, my_img_dy_np, big_Radon_img  
